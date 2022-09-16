2e990942e8e2b6ccaff1834691680364736a8400
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-09-16 15:12:50 -0300

    refactor: errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -5,11 +5,11 @@ import { ServerErrors } from '../helpers/httpStatus';
 const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
   const errorType = error.name as ErrorType;
   const httpStatus = ErrorCatalog[errorType] || ServerErrors.InternalServer;
+  let message = 'internal error';
   
-  if (errorType in ErrorCatalog) {
-    res.status(httpStatus).json({ message: error.message });
-  }
-  res.status(httpStatus).json({ message: 'internal error' });
+  if (errorType in ErrorCatalog) message = error.message;
+  
+  res.status(httpStatus).json({ message });
 };
 
 export default errorHandler;
