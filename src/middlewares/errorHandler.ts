3f626b72ce13aa8092a011db456b7b354fb45dff
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-09-21 12:09:33 -0300

    fix(errorHandler): corrige nome da chave do objeto com a mensagem de erro

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -5,11 +5,11 @@ import { ServerErrors } from '../helpers/httpStatus';
 const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
   const errorType = error.name as ErrorType;
   const httpStatus = error.statusCode || ServerErrors.InternalServer;
-  let message = 'internal error';
+  let errMessage = 'internal error';
   
-  if (errorType in ErrorCatalog) message = error.message;
+  if (errorType in ErrorCatalog) errMessage = error.message;
   
-  res.status(httpStatus).json({ message });
+  res.status(httpStatus).json({ error: errMessage });
 };
 
 export default errorHandler;
