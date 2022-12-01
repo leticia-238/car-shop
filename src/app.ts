70ffec43e6f22a72987075439e35ba87d0b0e10d
leticia-238 <leticiaoliveiraandrade.9@gmail.com>
2022-12-01 18:38:51 -0300

    📃 docs: Inicia documentação da api com swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import 'express-async-errors';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 import errorHandler from './middlewares/errorHandler';
 import { carsRouter } from './routes';
+import swaggerDocs from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(carsRouter);
 
